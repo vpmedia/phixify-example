f76a7a1722761b1fdbc5ef6e606c2750f387d8f7
Andras Csizmadia <andras@vpmedia.hu>
2022-12-02 13:32:21 +0100

    Added spectorjs debugger

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import "./style.css";
+import { Spector } from "spectorjs";
 import { getPhaserApp } from "./phaser/getPhaserApp.js";
 import { getPixiApp } from "./pixi/getPixiApp.js";
 
+window.spector = new Spector();
+window.spector.displayUI();
 window.pixiApp = getPixiApp();
 window.phaserApp = getPhaserApp();
