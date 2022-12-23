3b9d8a36663ac962d37a0de17889e3808e825492
Andras Csizmadia <andras@vpmedia.hu>
2022-12-23 18:09:40 +0100

    Improve build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import "./style.css";
-import { Spector } from "spectorjs";
 import FontFaceObserver from "fontfaceobserver";
 import { getPhaserApp } from "./phaser/getPhaserApp.js";
 import { getPixiApp } from "./pixi/getPixiApp.js";
+// import { Spector } from "spectorjs";
 
-window.spector = new Spector();
-window.spector.displayUI();
+// window.spector = new Spector();
+// window.spector.displayUI();
 
 new FontFaceObserver("Lineal").load().then(() => {
   window.pixiApp = getPixiApp();
