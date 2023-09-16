4abf14520ee1c1ce6b59cced04e1d379052ae332
Andras Csizmadia <andras@vpmedia.hu>
2023-09-16 13:27:40 +0200

    Migrate to *.config.js format

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("stylelint").Config} */
+const config = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "font-family-name-quotes": "always-unless-keyword",
+    "color-hex-length": "long"
+  }
+};
+
+export default config;
